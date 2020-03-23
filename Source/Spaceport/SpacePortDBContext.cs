8fdb0ce2f38d1729c33232711f9cafcd76d97340
oskar morell <oskarmorell1998@hotmail.se>
2020-03-23 15:35:05 +0100

    driver @oskarmorell

--- Source/Spaceport/SpacePortDBContext.cs
+++ Source/Spaceport/SpacePortDBContext.cs
@@ -1,8 +1,4 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Text;
 
 namespace Spaceport
 {
