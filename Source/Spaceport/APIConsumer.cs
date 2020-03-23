a0f65bf3e5ef3554707597395557f17cbe3d1e40
norsh_6pstx7m <norsh_6pstx7m@LAPTOP-J0PKJ9I1>
2020-03-23 14:41:12 +0100

    Driver @istorp

---
+++ Source/Spaceport/APIConsumer.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Spaceport
+{
+    class APIConsumer
+    {
+    }
+
+    public class CharacterData
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+    }
+    public class CharacterDataRoot
+    {
+        [JsonProperty("results")]
+        public List<CharacterData> Results { get; set; }
+
+    }
+}
