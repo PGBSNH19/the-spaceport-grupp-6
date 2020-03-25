eccdfdca1ff0390299b5e204a8792f1a7b75ec35
Pierre Nygård <pierre.nygard@gmail.com>
2020-03-25 09:29:44 +0100

    Added folders

--- Source/Spaceport/SpacePortDBContext.cs
+++ Source/Spaceport/SpacePortDBContext.cs
@@ -18,6 +18,11 @@ namespace Spaceport
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            //modelBuilder.Entity<Person>().HasData(
+            //    new Person(
+            //    )
+            //);
+                
             base.OnModelCreating(modelBuilder);
         }
     }
